4500f67f633b84772feef8cef3bd826c9e827086
Elias Volanakis <elias@eclipsesource.com>
2010-02-19 19:01:24 +0000

    302897: [RoR] Port / Single-Source ridgets

---
+++ org.eclipse.riena.ui.swt.rap/src/org/eclipse/riena/ui/swt/facades/SWTFacadeImpl.java
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ * Copyright (c) 2007, 2009 compeople AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    compeople AG - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.riena.ui.swt.facades;
+
+import java.util.EventListener;
+
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.Tree;
+
+/**
+ * TODO [ev] docs
+ */
+public class SWTFacadeImpl extends SWTFacade {
+
+	@Override
+	public void addEraseItemListener(Table table, Listener listener) {
+		// do nothing
+	}
+
+	@Override
+	public void addEraseItemListener(Tree tree, Listener listener) {
+		// do nothing
+	}
+
+	@Override
+	public void addPaintItemListener(Tree tree, Listener listener) {
+		// do nothing
+	}
+
+	@Override
+	public void addPaintListener(Control control, EventListener listener) {
+		// do nothing
+	}
+
+	@Override
+	public EventListener createDisabledPainter() {
+		return null;
+	}
+
+	@Override
+	public Listener createTableItemEraser() {
+		return null;
+	}
+
+	@Override
+	public Listener createTreeItemEraserAndPainter() {
+		return null;
+	}
+
+	@Override
+	public void removeEraseItemListener(Table table, Listener listener) {
+		// do nothing
+	}
+
+	@Override
+	public void removeEraseItemListener(Tree tree, Listener listener) {
+		// do nothing
+	}
+
+	@Override
+	public void removePaintItemListener(Tree tree, Listener listener) {
+		// do nothing
+	}
+
+	@Override
+	public void removePaintListener(Control control, EventListener listener) {
+		// do nothing
+	}
+
+}
