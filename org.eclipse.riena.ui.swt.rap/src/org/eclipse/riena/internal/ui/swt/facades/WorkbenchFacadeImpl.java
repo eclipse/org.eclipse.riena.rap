c0986f6d5c8ea946c5c55e99af94c04dd46202d1
Thorsten Schenkel <thorsten.schenkel@compeople.de>
2013-06-05 08:50:06 +0200

    implementation of getSourceProviders added to RAP facade

--- org.eclipse.riena.ui.swt.rap/src/org/eclipse/riena/internal/ui/swt/facades/WorkbenchFacadeImpl.java
+++ org.eclipse.riena.ui.swt.rap/src/org/eclipse/riena/internal/ui/swt/facades/WorkbenchFacadeImpl.java
@@ -10,17 +10,26 @@
  *******************************************************************************/
 package org.eclipse.riena.internal.ui.swt.facades;
 
+import org.osgi.service.log.LogService;
+
+import org.eclipse.equinox.log.Logger;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.ISourceProvider;
 import org.eclipse.ui.IViewReference;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.internal.WorkbenchPage;
+import org.eclipse.ui.services.ISourceProviderService;
+
+import org.eclipse.riena.core.Log4r;
 
 /**
  * RAP specific implementation.
  */
 public class WorkbenchFacadeImpl extends WorkbenchFacade {
+
+	private static final Logger LOGGER = Log4r.getLogger(WorkbenchFacadeImpl.class);
 
 	/*
 	 * (non-Javadoc)
@@ -86,6 +95,25 @@ public class WorkbenchFacadeImpl extends WorkbenchFacade {
 			return PlatformUI.getWorkbench().getDisplay();
 		} else {
 			return null;
+		}
+	}
+
+	private ISourceProviderService getSourceProviderService() {
+		return (ISourceProviderService) PlatformUI.getWorkbench().getService(ISourceProviderService.class);
+	}
+
+	@Override
+	public ISourceProvider[] getSourceProviders() {
+		try {
+			final ISourceProviderService sourceProviderService = getSourceProviderService();
+			if (sourceProviderService == null) {
+				return new ISourceProvider[0];
+			} else {
+				return sourceProviderService.getSourceProviders();
+			}
+		} catch (final IllegalStateException ex) {
+			LOGGER.log(LogService.LOG_ERROR, "No service for ISourceProviderService!", ex); //$NON-NLS-1$
+			return new ISourceProvider[0];
 		}
 	}
 
